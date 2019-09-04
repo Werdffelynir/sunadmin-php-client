973d150a3b692d51dd0843c98089c78a02323d63
Werdffelynir <werdffelynir@gmail.com>
2019-09-04 19:37:48 +0300

    update sld-data

--- src/SunLightData.php
+++ src/SunLightData.php
@@ -21,6 +21,8 @@ class SunLightData implements PrepareData
              'about' => [
                  'title' => null,
                  'content' => null,
+                 'phone' => null,
+                 'email' => null,
                  'list' => [],
              ],
              'wedo' => [
@@ -44,10 +46,16 @@ class SunLightData implements PrepareData
                 switch ($item->name) {
                     case 'header':
                     case 'intro':
-                    case 'about':
                         $return[$item->name]['title'] = $item->title;
                         $return[$item->name]['content'] = $item->body;
-                        break;
+                    break;
+                    case 'about':
+                        $options = APIHelper::optionDecode($item);
+                        $return[$item->name]['title'] = $item->title;
+                        $return[$item->name]['content'] = $options; // $item->body;
+                        $return[$item->name]['phone'] = $options->phone;
+                        $return[$item->name]['email'] = $options->email;
+                     break;
                 }
 
                 if (strstr($item->name, 'job-')) {
